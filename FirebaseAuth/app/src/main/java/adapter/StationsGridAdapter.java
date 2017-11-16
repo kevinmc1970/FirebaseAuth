4acd464fe71ca4f6313b3d5561c9a279561bb654
kevinmc1970 <mcintech@gmail.com>
2017-11-16 20:50:16 +0000

    Got scroll to work in gridView. it should be automatically included and it is but think I had to increase the linearlayout height too (i.e. match_parent)

--- FirebaseAuth/app/src/main/java/adapter/StationsGridAdapter.java
+++ FirebaseAuth/app/src/main/java/adapter/StationsGridAdapter.java
@@ -2,15 +2,17 @@ package adapter;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Date;
 import java.util.List;
 
+import info.androidhive.firebase.R;
 import info.androidhive.model.Station;
 
 public class StationsGridAdapter extends BaseAdapter {
@@ -45,26 +47,32 @@ public class StationsGridAdapter extends BaseAdapter {
         // 5
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
+            // need to set this once along with the View I think
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            RelativeLayout r = (RelativeLayout) inflater.inflate(R.layout.station_layout, parent, false);
+
             Station station = getItem(position);
-            TextView dummyTextView = new TextView(mContext);
-            TextView name = new TextView(mContext);
-            TextView available = new TextView(mContext);
-            TextView working = new TextView(mContext);
-            LinearLayout r = new LinearLayout(mContext);
-            r.setOrientation(LinearLayout.VERTICAL);
-            r.addView(name);
-            r.addView(dummyTextView);
-            r.addView(available);
-            r.addView(working);
-            dummyTextView.setText(station.getUserName());
+//            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+//            params.addRule(RelativeLayout., RelativeLayout.TRUE);
+            TextView name = (TextView) r.findViewById(R.id.station_name);
+            TextView available = (TextView) r.findViewById(R.id.station_available);
+            TextView user = (TextView) r.findViewById(R.id.station_user);
+
             name.setText(station.getName());
+            user.setText(station.getUserName());
+            // green if working and not in use
+            r.setBackgroundColor(Color.parseColor("#64FFDA"));
+            available.setText("Charger Available for Use");
             if (station.getAvailable() > 0) {
-                available.setText(String.valueOf(new Date(getItem(position).getAvailable())));
+                available.setText(String.valueOf(new Date(station.getAvailable())));
+                // blue if in use
+                r.setBackgroundColor(Color.parseColor("#FF9100"));
             }
-            if (station.getWorking()) {
-                working.setText("charger working");
+            if (!station.getWorking()) {
+                // grey if in broken
+                r.setBackgroundColor(Color.parseColor("#BDBDBD"));
+                available.setText("Charger Broken");
             }
-            r.setBackgroundColor(Color.parseColor("#d9d5dc"));
             return r;
         }
 
